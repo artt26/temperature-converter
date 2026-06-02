da8d49ede3645995823d5b4e7d87d1ba7945ff63
Arthur Azevedo <azevedoarthur2610@gmail.com>
2026-06-02 19:40:01 -0300

    Kelvin conversion added

--- script.js
+++ script.js
@@ -1,17 +1,38 @@
 const textbox = document.getElementById("textbox")
-const toF = document.getElementById("toF")
-const toC = document.getElementById("toC")
 const result = document.getElementById("result")
+
+const CtoF = document.getElementById("CtoF")
+const FtoC = document.getElementById("FtoC")
+const CtoK = document.getElementById("CtoK")
+const FtoK = document.getElementById("FtoK")
+const KtoF = document.getElementById("KtoF")
+const KtoC = document.getElementById("KtoC")
 
 let temp
 
 function convert(){
-    if(toF.checked){
+    if(CtoF.checked){
         temp = Number(textbox.value) * 9 / 5 + 32
         result.textContent = temp.toFixed(2) + "°F"  
     }
-    else if(toC.checked){
+    else if(FtoC.checked){
         temp = (Number(textbox.value) - 32) * 5 / 9
+        result.textContent = temp.toFixed(2) + "°C"
+    }
+    else if(CtoK.checked){
+        temp = Number(textbox.value) + 273.15
+        result.textContent = temp.toFixed(2) + "K"
+    }
+    else if(FtoK.checked){
+        temp = (Number(textbox.value) - 32) * 5 / 9 + 273.15
+        result.textContent = temp.toFixed(2) + "K"
+    }
+    else if(KtoF.checked){
+        temp = (Number(textbox.value) - 273.15) * 9 / 5 + 32
+        result.textContent = temp.toFixed(2) + "°F"
+    }
+    else if(KtoC.checked){
+        temp = Number(textbox.value) - 273.15
         result.textContent = temp.toFixed(2) + "°C"
     }
     else{
